59cafc09443fb6a569f58b1153b2dc3b255147f3
nguyenkhanh1511 <nguyenkhanhsapa1511@gmail.com>
2026-09-16 12:14:14 +0700

    Add files via upload

---
+++ BT4.2/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace BT4._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtName = new TextBox();
+            mtxtPhone = new MaskedTextBox();
+            dtpBirthDate = new DateTimePicker();
+            cboCourse = new ComboBox();
+            rdoMale = new RadioButton();
+            rdoFemale = new RadioButton();
+            chkCSharp = new CheckBox();
+            chkJava = new CheckBox();
+            chkPython = new CheckBox();
+            btnRegister = new Button();
+            chkScholarship = new CheckBox();
+            SuspendLayout();
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(72, 12);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(125, 27);
+            txtName.TabIndex = 0;
+            // 
+            // mtxtPhone
+            // 
+            mtxtPhone.Location = new Point(72, 58);
+            mtxtPhone.Mask = "(000) 000-0000";
+            mtxtPhone.Name = "mtxtPhone";
+            mtxtPhone.Size = new Size(125, 27);
+            mtxtPhone.TabIndex = 1;
+            // 
+            // dtpBirthDate
+            // 
+            dtpBirthDate.CustomFormat = "dd/MM/yyyy";
+            dtpBirthDate.Format = DateTimePickerFormat.Custom;
+            dtpBirthDate.Location = new Point(72, 110);
+            dtpBirthDate.Name = "dtpBirthDate";
+            dtpBirthDate.Size = new Size(250, 27);
+            dtpBirthDate.TabIndex = 2;
+            // 
+            // cboCourse
+            // 
+            cboCourse.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboCourse.FormattingEnabled = true;
+            cboCourse.Location = new Point(72, 155);
+            cboCourse.Name = "cboCourse";
+            cboCourse.Size = new Size(151, 28);
+            cboCourse.TabIndex = 3;
+            // 
+            // rdoMale
+            // 
+            rdoMale.AutoSize = true;
+            rdoMale.Location = new Point(72, 198);
+            rdoMale.Name = "rdoMale";
+            rdoMale.Size = new Size(62, 24);
+            rdoMale.TabIndex = 4;
+            rdoMale.TabStop = true;
+            rdoMale.Text = "Nam";
+            rdoMale.UseVisualStyleBackColor = true;
+            // 
+            // rdoFemale
+            // 
+            rdoFemale.AutoSize = true;
+            rdoFemale.Location = new Point(140, 198);
+            rdoFemale.Name = "rdoFemale";
+            rdoFemale.Size = new Size(50, 24);
+            rdoFemale.TabIndex = 5;
+            rdoFemale.TabStop = true;
+            rdoFemale.Text = "Nữ";
+            rdoFemale.UseVisualStyleBackColor = true;
+            // 
+            // chkCSharp
+            // 
+            chkCSharp.AutoSize = true;
+            chkCSharp.Location = new Point(72, 242);
+            chkCSharp.Name = "chkCSharp";
+            chkCSharp.Size = new Size(49, 24);
+            chkCSharp.TabIndex = 6;
+            chkCSharp.Text = "C#";
+            chkCSharp.UseVisualStyleBackColor = true;
+            // 
+            // chkJava
+            // 
+            chkJava.AutoSize = true;
+            chkJava.Location = new Point(170, 242);
+            chkJava.Name = "chkJava";
+            chkJava.Size = new Size(59, 24);
+            chkJava.TabIndex = 7;
+            chkJava.Text = "Java";
+            chkJava.UseVisualStyleBackColor = true;
+            // 
+            // chkPython
+            // 
+            chkPython.AutoSize = true;
+            chkPython.Location = new Point(277, 242);
+            chkPython.Name = "chkPython";
+            chkPython.Size = new Size(76, 24);
+            chkPython.TabIndex = 8;
+            chkPython.Text = "Python";
+            chkPython.UseVisualStyleBackColor = true;
+            // 
+            // btnRegister
+            // 
+            btnRegister.Location = new Point(157, 290);
+            btnRegister.Name = "btnRegister";
+            btnRegister.Size = new Size(94, 29);
+            btnRegister.TabIndex = 9;
+            btnRegister.Text = "Đăng ký";
+            btnRegister.UseVisualStyleBackColor = true;
+            btnRegister.Click += btnRegister_Click;
+            // 
+            // chkScholarship
+            // 
+            chkScholarship.AutoSize = true;
+            chkScholarship.Location = new Point(221, 198);
+            chkScholarship.Name = "chkScholarship";
+            chkScholarship.Size = new Size(133, 24);
+            chkScholarship.TabIndex = 10;
+            chkScholarship.Text = "Nhận học bổng";
+            chkScholarship.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(chkScholarship);
+            Controls.Add(btnRegister);
+            Controls.Add(chkPython);
+            Controls.Add(chkJava);
+            Controls.Add(chkCSharp);
+            Controls.Add(rdoFemale);
+            Controls.Add(rdoMale);
+            Controls.Add(cboCourse);
+            Controls.Add(dtpBirthDate);
+            Controls.Add(mtxtPhone);
+            Controls.Add(txtName);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            KeyUp += Form1_KeyUp;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtName;
+        private MaskedTextBox mtxtPhone;
+        private DateTimePicker dtpBirthDate;
+        private ComboBox cboCourse;
+        private RadioButton rdoMale;
+        private RadioButton rdoFemale;
+        private CheckBox chkCSharp;
+        private CheckBox chkJava;
+        private CheckBox chkPython;
+        private Button btnRegister;
+        private CheckBox chkScholarship;
+    }
+}
---
+++ BT4.2/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace BT4._2
+{
+    // 1. ĐẶT FORM1 LÊN ĐẦU FILE
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mtxtPhone.Mask = "(000) 000-0000";
+            dtpBirthDate.Format = DateTimePickerFormat.Custom;
+            dtpBirthDate.CustomFormat = "dd/MM/yyyy";
+
+            List<Course> listCourses = new List<Course>
+            {
+                new Course("CS01", "Lập trình C# WinForms"),
+                new Course("CS02", "Lập trình Web ASP.NET Core"),
+                new Course("CS03", "Phân tích dữ liệu với Python"),
+                new Course("CS04", "Thiết kế CSDL SQL Server")
+            };
+
+            cboCourse.DataSource = listCourses;
+            cboCourse.DisplayMember = "CourseName";
+            cboCourse.ValueMember = "CourseId";
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text))
+            {
+                MessageBox.Show("Vui lòng nhập họ và tên!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtName.Focus();
+                return;
+            }
+
+            if (!mtxtPhone.MaskCompleted)
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ số điện thoại!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                mtxtPhone.Focus();
+                return;
+            }
+
+            string gender = rdoMale.Checked ? "Nam" : "Nữ";
+            string birthDate = dtpBirthDate.Value.ToString("dd/MM/yyyy");
+            string selectedCourseName = cboCourse.Text;
+            string selectedCourseId = cboCourse.SelectedValue?.ToString() ?? "";
+            string scholarshipStatus = chkScholarship.Checked ? "Có" : "Không";
+
+            string info = $"THÔNG TIN ĐĂNG KÝ HỌC\n" +
+                          $"--------------------------------\n" +
+                          $"Họ và tên: {txtName.Text.Trim()}\n" +
+                          $"Ngày sinh: {birthDate}\n" +
+                          $"Giới tính: {gender}\n" +
+                          $"Số điện thoại: {mtxtPhone.Text}\n" +
+                          $"Khóa học: {selectedCourseName} (Mã: {selectedCourseId})\n" +
+                          $"Nhận học bổng: {scholarshipStatus}";
+
+            MessageBox.Show(info, "Kết quả đăng ký", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+
+    // 2. CHUYỂN CLASS COURSE XUỐNG DƯỚI NÀY
+    public class Course
+    {
+        public string CourseId { get; set; }
+        public string CourseName { get; set; }
+
+        public Course(string id, string name)
+        {
+            CourseId = id;
+            CourseName = name;
+        }
+    }
+}
